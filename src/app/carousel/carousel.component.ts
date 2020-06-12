f2962449b5d15bf24021be113170f1f8b912a780
ckoske <koske.hannah@gmail.com>
2020-06-12 21:25:17 +0300

    Add carousel component

---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.pug',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
