ee1130d420f751540e93a6446af31bb6e42b8566
ckoske <koske.hannah@gmail.com>
2020-06-14 04:40:01 +0300

    Add items route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeViewComponent } from './home-view/home-view.component';
+import { ItemsViewComponent } from './items-view/items-view.component';
 
 
 const routes: Routes = [
   { path: '',   component: HomeViewComponent, pathMatch: 'full' }, 
+  { path: 'items/:category', component: ItemsViewComponent },
   { path: '**', component: HomeViewComponent },
 ];
 
