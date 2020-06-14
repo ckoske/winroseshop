fabcac1622e98d3989f4c74a87051793e01411aa
ckoske <koske.hannah@gmail.com>
2020-06-14 16:57:02 +0300

    Add content to items view

--- src/app/items-view/items-view.component.ts
+++ src/app/items-view/items-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { INavCrumb } from '../nav/navcrumb';
+
 
 @Component({
   selector: 'app-items-view',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./items-view.component.css']
 })
 export class ItemsViewComponent implements OnInit {
+  category: string;
+  
+  crumbs: INavCrumb[] = [
+    {text: "Home", href: "/"}
+  ];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-  }
+    this.route.paramMap.subscribe( paramMap => {
+      this.category = paramMap.get('category');
 
+      if(this.category){
+        this.crumbs.push({text: "Items", href: "/items"});
+      } else {
+        this.category = 'Items';
+      }
+    });
+  }
 }
